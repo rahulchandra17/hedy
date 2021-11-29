6a668d13a07053eec7f1ebd244164d9e389ba947
s2406403 <h.e.sprangers@umail.leidenuniv.nl>
2021-11-29 02:43:42 +0100

    keychange generation changes

--- static/js/keychange.sh
+++ static/js/keychange.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Replaces the keywords from syntaxKeywords.ts to be used in syntaxModesRules.ts to the ones listed in the appropriate language.
+#Takes the keywords from grammars/keywords-'lang'.lark and creates a syntaxModesRules-'lang'.ts file for that language
 #How to use: add the argument for which language you want when using the script, e.g. 'keychange.sh en' for english
 
 set -eu
@@ -8,10 +8,12 @@ cd $scriptdir
 #ls
 
 echo "//Generated through keychange.sh
-" > syntaxKeywords.ts
+" > syntaxModesRules-$1.ts
 
 cat ../../grammars/keywords-$1.lark | while read line; do
   newline=$(echo ${line/:/=})
-  echo var ${newline/\r?\n|\r/}';' >> syntaxKeywords.ts
+  echo var ${newline/\r?\n|\r/}';' >> syntaxModesRules-$1.ts
 done
+
+cat ./syntaxModesRules.ts >> syntaxModesRules-$1.ts
 
